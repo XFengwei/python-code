6b3113c8255bd81926ffab1fa9fc343c0f72ef4f
XU FENGWEI <47975433+XFengwei@users.noreply.github.com>
2021-08-27 18:05:21 +0800

    Update scReproject.py

--- scReproject.py
+++ scReproject.py
@@ -1,1 +1,15 @@
+######################################################################
+# JCMT usually offers data with a rotation of specific angle, relative
+# to world coordinates (Ra. & Dec.)
+######################################################################
+
+from astropy import units as u
+import numpy as np
+from astropy import wcs
+from spectral_cube import SpectralCube as sc
+from reproject import reproject_interp
+
+input = ''
+sc.read(,hdu=0)
+
 
